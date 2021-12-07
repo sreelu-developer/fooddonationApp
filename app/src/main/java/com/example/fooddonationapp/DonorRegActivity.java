7afd60ddc136ef36f9561166307ea7fb059c639f
Sreelakshmi.P.S <pssreelakshmi.sps@gmail.com>
2021-12-07 11:18:27 +0530

    donor registration form

---
+++ app/src/main/java/com/example/fooddonationapp/DonorRegActivity.java
@@ -0,0 +1,15 @@
+package com.example.fooddonationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonorRegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_reg);
+
+    }
+}
--- app/src/main/java/com/example/fooddonationapp/UserlistActivity.java
+++ app/src/main/java/com/example/fooddonationapp/UserlistActivity.java
@@ -1,14 +1,26 @@
 package com.example.fooddonationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class UserlistActivity extends AppCompatActivity {
-
+    CardView donor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userlist);
+        donor = findViewById(R.id.cardDonor);
+        donor.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // redirect to RegisterActivity
+                Intent intent = new Intent(getApplicationContext(), DonorRegActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
