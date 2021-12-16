122f8e75ff9ceed4a6f87fdc23d87058b2a46319
siraj-m-s <86203332+siraj-m-s@users.noreply.github.com>
2021-12-17 01:15:13 +0530

    Add files via upload
    VOLUNTEER MODULE : UPDATE STATUS JAVA FILE

---
+++ app/src/main/java/com/example/fooddonationapp/UpdateStatusActivity.java
@@ -0,0 +1,14 @@
+package com.example.fooddonationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_status);
+    }
+}
